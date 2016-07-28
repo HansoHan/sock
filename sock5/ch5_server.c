e39b98c8e31ed64fc639bfd2849f4bd6d056e63f
HansoHan <bluehating@gmail.com>
2016-07-28 20:49:59 +0900

    chapter5, problem 5, server program

---
+++ sock5/ch5_server.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#define BUF_SIZE 1024
+#define STR_LEN 4
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+	int serv_sock, clnt_sock;
+	char message[BUF_SIZE];
+	struct sockaddr_in serv_adr, clnt_adr;
+	socklen_t clnt_adr_sz;
+	
+	if(argc!=2){
+		printf("Usage : %s <port> \n", argv[0]);
+		exit(1);
+	}
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+	if(serv_sock==-1)
+		error_handling("socket() error");
+
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family=AF_INET;
+	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_adr.sin_port=htons(atoi(argv[1]));
+
+	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+		error_handling("bind() error");
+
+	if(listen(serv_sock, 1)==-1)
+		error_handling("listen() error");
+
+	clnt_adr_sz=sizeof(clnt_adr);
+	clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+	if(clnt_sock==-1)
+		error_handling("accept() error");
+
+	printf("Input usage : <data size> <message> \n");
+       	printf("Input message 1 : ");
+	scanf("%d %s", (int*)message, (char*)((int*)message+1));
+	write(clnt_sock, message, strlen(message));
+
+	close(clnt_sock);
+	close(serv_sock);
+	return 0;
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+	
+
+
+
+
+
+
+
+
